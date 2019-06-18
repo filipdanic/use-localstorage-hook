21b0b8c07a6e1b5274c2ade3fa2f7e80f0df7530
Filip Danić <filip@spicefactory.co>
2019-06-18 14:28:08 +0200

    Updates

---
+++ precache-manifest.1064485ba199130757484fafe328b10c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0393a6d5366b311c541797fbc3d0280",
+    "url": "/use-localstorage-hook/index.html"
+  },
+  {
+    "revision": "7b70f884a0b4d486bc66",
+    "url": "/use-localstorage-hook/static/css/main.8b9b1f47.chunk.css"
+  },
+  {
+    "revision": "63a99bf8e5a308371c94",
+    "url": "/use-localstorage-hook/static/js/2.4723ed4c.chunk.js"
+  },
+  {
+    "revision": "7b70f884a0b4d486bc66",
+    "url": "/use-localstorage-hook/static/js/main.f0eee390.chunk.js"
+  },
+  {
+    "revision": "94180d5fe166083e4761",
+    "url": "/use-localstorage-hook/static/js/runtime~main.50978605.js"
+  },
+  {
+    "revision": "95c36367f0ba91a1b3d17a818aeb769d",
+    "url": "/use-localstorage-hook/static/media/logo.95c36367.png"
+  }
+]);
